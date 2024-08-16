dd75c9c45c89c7d085c5b6614569a1526ff0563f
Pooya Tolideh <pooyatolideh@Pooyas-Laptop.local>
2024-08-16 13:23:25 -0400

    organize app file

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from '../components/App/App'; // Adjust the import path as necessary
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "../components/App"; // Adjust the import path as necessary
 
-describe('App component', () => {
-  test('renders Vite and React logos', () => {
+describe("App component", () => {
+  test("renders Vite and React logos", () => {
     render(<App />);
 
-    const viteLogo = screen.getByAltText('Vite logo');
-    const reactLogo = screen.getByAltText('React logo');
+    const viteLogo = screen.getByAltText("Vite logo");
+    const reactLogo = screen.getByAltText("React logo");
 
     expect(viteLogo).toBeInTheDocument();
     expect(reactLogo).toBeInTheDocument();
   });
 
-  test('increments count when button is clicked', () => {
+  test("increments count when button is clicked", () => {
     render(<App />);
 
-    const countButton = screen.getByRole('button')
+    const countButton = screen.getByRole("button");
     fireEvent.click(countButton);
 
     expect(screen.getByText(/count is\s1/)).toBeInTheDocument();
   });
 
-  test('renders links to Vite and React websites', () => {
+  test("renders links to Vite and React websites", () => {
     render(<App />);
 
-    const viteLink = screen.getByRole('link', { name: /vite/i });
-    const reactLink = screen.getByRole('link', { name: /react/i });
+    const viteLink = screen.getByRole("link", { name: /vite/i });
+    const reactLink = screen.getByRole("link", { name: /react/i });
 
     expect(viteLink).toBeInTheDocument();
     expect(reactLink).toBeInTheDocument();
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App.tsx";
 import "./reset.css";
-import "modern-normalize/modern-normalize.css";
+// import "modern-normalize/modern-normalize.css";
 import "./styles.css";
+
+import App from "./components/App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
