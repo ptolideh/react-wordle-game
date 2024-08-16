da79473234c346b18b224302e4c31108bedf1dde
Pooya Tolideh <pooyatolideh@Pooyas-Laptop.local>
2024-08-16 09:59:22 -0400

    added styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App.tsx'
-import 'modern-normalize/modern-normalize.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App.tsx";
+import "./reset.css";
+import "modern-normalize/modern-normalize.css";
+import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
